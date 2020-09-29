c7ff990d8bd5d81cd3ae8900d2acc1929da81e70
aga <aga3134@gmail.com>
2020-09-29 16:57:20 +0800

    update cam capture node to use gstreamer

--- nano/ros_ws/src/vapaa_cruiser/src/cam_capture_node.py
+++ nano/ros_ws/src/vapaa_cruiser/src/cam_capture_node.py
@@ -14,18 +14,35 @@ class CamCapture():
         self.width = rospy.get_param("~width",640)
         self.height = rospy.get_param("~height",480)
         self.rate = rospy.get_param("~rate",30)
-        self.flipH = rospy.get_param("~flipH",False)
-        self.flipV = rospy.get_param("~flipV",False)
+        self.flipMode = rospy.get_param("~flipMode",0)
         rospy.loginfo("use camera id=%d, w=%d, h=%d, rate=%f" % (self.camID,self.width,self.height,self.rate))
         
-        self.cap = cv2.VideoCapture(self.camID)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        self.cap = cv2.VideoCapture(self.gstreamer_pipeline(), cv2.CAP_GSTREAMER)
         self.br = CvBridge()
         self.pub = rospy.Publisher("image/compressed",CompressedImage,queue_size=1)
         self.sub = rospy.Subscriber("image/capture",Empty,self.CaptureImage)
         self.savePath = rospy.get_param("~savePath","captureImage/")
         self.frame = None
+
+    def gstreamer_pipeline(self):
+        return (
+            "nvarguscamerasrc ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)%d, height=(int)%d, "
+            "format=(string)NV12, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink"
+            % (
+                self.width,
+                self.height,
+                self.rate,
+                self.flipMode,
+                self.width,
+                self.height,
+            )
+        )
 
     def CaptureImage(self,msg):
         if self.frame is not None:
@@ -41,12 +58,6 @@ class CamCapture():
         rate = rospy.Rate(self.rate)
         while not rospy.is_shutdown():
             ret, self.frame = self.cap.read()
-            if self.flipH and self.flipV:
-                self.frame = cv2.flip(self.frame,-1)
-            elif self.flipH:
-                self.frame = cv2.flip(self.frame,1)
-            elif self.flipV:
-                self.frame = cv2.flip(self.frame,0)
             msg = self.br.cv2_to_compressed_imgmsg(self.frame)
             self.pub.publish(msg)
             #cv2.imshow('frame',self.frame)
