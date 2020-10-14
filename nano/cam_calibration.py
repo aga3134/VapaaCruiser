f66b5b306126ae8c7b22933e2439bca05dbc84c5
aga <aga3134@gmail.com>
2020-10-14 15:57:17 +0800

    add apriltag pose estimation test

--- nano/cam_calibration.py
+++ nano/cam_calibration.py
@@ -1,23 +1,24 @@
 import cv2
 import numpy as np
+import yaml
 
 gridX = 8
 gridY = 6
-gridSize = 18
-targetW = 320
-targetH = 240
+gridSize = 25
+targetW = 640
+targetH = 480
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 cap = cv2.VideoCapture(2)
 
+imageNum = 0
 objPtArr = []
 imagePtArr = []
 objPt = np.zeros((gridX*gridY,3), np.float32)
-for x in range(gridX):
-    for y in range(gridY):
-        objPt[x*gridY+y] = [x*gridSize,y*gridSize,0]
+for y in range(gridY):
+    for x in range(gridX):
+        objPt[y*gridX+x] = [x*gridSize,y*gridSize,0]
 
-imageNum = 0
 while(True):
     ret, frame = cap.read()
     frame = cv2.resize(frame, (targetW, targetH), interpolation=cv2.INTER_CUBIC)
@@ -42,12 +43,34 @@ while(True):
         else:
             print("no chess board found")
     elif key == ord("c"): #do calibration
-        ret, mat, distort, rotate, translate = cv2.calibrateCamera(objPtArr, imagePtArr, gray.shape[::-1],None,None)
-        print(mat)
-        print(distort)
-        print(rotate)
-        print(translate)
+        if imageNum < 10:
+            print("please add at least 10 chessboard images with different positions & angles")
+        else:
+            ret, mat, distort, rotate, translate = cv2.calibrateCamera(objPtArr, imagePtArr, gray.shape[::-1],None,None)
+            output = {
+                "image_width": targetW,
+                "image_height": targetH,
+                "camera_matrix": {
+                    "rows": mat.shape[0],
+                    "cols": mat.shape[1],
+                    "data": mat.flatten().tolist()
+                },
+                "distortion_coefficients": {
+                    "rows": distort.shape[0],
+                    "cols": distort.shape[1],
+                    "data": distort.flatten().tolist()
+                },
+            }
+            filename = "calibration.yml"
+            with open(filename, "w") as f:
+                yaml.dump(output, f)
+                print(output)
+                print("save calibration result to %s" % filename)
 
+            #reset 
+            imageNum = 0
+            objPtArr = []
+            imagePtArr = []
 
 cap.release()
 cv2.destroyAllWindows()
--- nano/opencv_apriltag.py
+++ nano/opencv_apriltag.py
@@ -1,11 +1,27 @@
 import apriltag
 import cv2
+import yaml
+
+tagSize = 30
+with open("calibration.yml", "r") as f:
+    calib = yaml.load(f)
+    print(calib)
+    camParam = [
+        calib["camera_matrix"]["data"][0],
+        calib["camera_matrix"]["data"][4],
+        calib["camera_matrix"]["data"][2],
+        calib["camera_matrix"]["data"][5],
+    ]
+    targetW = calib["image_width"]
+    targetH = calib["image_height"]
 
 cap = cv2.VideoCapture(2)
 detector = apriltag.Detector(apriltag.DetectorOptions(families="tag36h11") )
 
 while(True):
     ret, frame = cap.read()
+    frame = cv2.resize(frame, (targetW, targetH), interpolation=cv2.INTER_CUBIC)
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     tags = detector.detect(gray)
     for tag in tags:
@@ -13,6 +29,10 @@ while(True):
         cv2.line(frame, tuple(tag.corners[1].astype(int)), tuple(tag.corners[2].astype(int)), (0, 0, 255), 2)
         cv2.line(frame, tuple(tag.corners[2].astype(int)), tuple(tag.corners[3].astype(int)), (255, 0, 0), 2)
         cv2.line(frame, tuple(tag.corners[3].astype(int)), tuple(tag.corners[0].astype(int)), (255, 0, 0), 2)
+
+        pose, e0, e1 = detector.detection_pose(tag,camParam,tagSize)
+        distZ = pose[2][3]
+        cv2.putText(frame, "{:.2f}".format(distZ), tuple(tag.center.astype(int)), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 255), 1, cv2.LINE_AA)
 
     cv2.imshow('frame', frame)
 
