98fd89d45d743031cbe6e8b4050ac8fbf4509816
aga3134 <aga3134@gmail.com>
2020-12-08 17:17:01 +0800

    add pull down to uart pin to avoid boot0 pin boot system from memory

--- stm32/VapaaCruiser/Src/main.c
+++ stm32/VapaaCruiser/Src/main.c
@@ -168,7 +168,7 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 		
-		//Ëã•uartÁï∂ÊéâÔºåÈáçÊñ∞init
+		//?ã•uart?ï∂??âÔ?åÈ?çÊñ∞init
 		if(huart1.gState == HAL_UART_STATE_RESET || huart1.RxState == HAL_UART_STATE_RESET){
 			MX_USART1_UART_Init();
 		}
@@ -200,11 +200,11 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
+  /** Configure the main internal regulator output voltage 
   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
@@ -214,7 +214,7 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -602,7 +602,7 @@ void Error_Handler(void)
   * @retval None
   */
 void assert_failed(uint8_t *file, uint32_t line)
-{
+{ 
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
